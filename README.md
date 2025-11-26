fe36ee564acbda77a7edda0767dfda11593e92db
Steglod <steffen.glodedata@gmail.com>
2025-11-26 10:15:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GHP-Undervisning-18
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Steglod/GHP-Undervisning-18)
---
+++ script.js
@@ -0,0 +1,4 @@
+const fruit = 'Apple';
+const banana = 'Banana';
+// Basic Array (Flat Array) = verdier som er ulike datatyper
+const fruits = ['Apple', 'Banana', 'Orange', 'Peach'];
